d06e9c02c416578fdf32281800084656dfa05b32
Casey Greene <c@seygreene.com>
2015-09-06 22:39:51 -0700

    added glyphicons YAY and changed root path

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -11,7 +11,9 @@
 // about supported directives.
 //
 //= require jquery
+//= require jquery.turbolinks
 //= require jquery_ujs
-//= require bootstrap-sprockets
-//= require turbolinks
+//= require bootstrap
+//= require masonry/jquery.masonry
+//= require masonry/jquery.imagesloaded.min
 //= require_tree .
